cff7da00d33e3681507e169f550f4ed58c8f3cdf
Massimiliano <m.croci.ly@gmail.com>
2021-03-01 15:44:02 +0100

    keep styling the exercise

--- index.php
+++ index.php
@@ -18,23 +18,34 @@ include 'db.php'; // includiamo un file separato sempre php
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
   <link rel="stylesheet" href="css/style.css">
-  <title>Document</title>
+  <title>dischi musicali con php</title>
 </head>
 <body>
-  <?php
-  foreach($dischi as $disco){
-    // var_dump($disco);
-    $img = $disco['img'];
-    $name = $disco['name'];
-    $author = $disco['author'];
-    $date = $disco['date'];
-  ?>
-  <div class="">
-    <img src="<?php echo $img ?>" alt="<?php echo $name ?>">
+  <header>
 
-  </div>
+  </header>
 
-<?php } ?>
+
+  <main>
+    <div class="container">
+      <?php
+      foreach($dischi as $disco){
+        // var_dump($disco);
+        $img = $disco['img'];
+        $name = $disco['name'];
+        $author = $disco['author'];
+        $date = $disco['date'];
+      ?>
+      <div class="disco">
+        <img src="<?php echo $img ?>" alt="<?php echo $name ?>">
+
+      </div>
+
+    <?php } ?>
+
+    </div>
+
+  </main>
 
 
   <script src="js/script.js" charset="utf-8"></script>
