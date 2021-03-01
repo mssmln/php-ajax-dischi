486c09f50329b7cf9e10fc4f9208cf121ba8d90a
Massimiliano <m.croci.ly@gmail.com>
2021-03-01 14:20:11 +0100

    init

---
+++ index.php
@@ -0,0 +1,21 @@
+<!-- Prima Milestone:
+Stampare a schermo i dischi musicali solo con l’utilizzo di PHP.
+Al caricamento della pagina si dovranno visualizzare tutti i dischi.
+Utilizzare:
+Html, Sass, PHP nella milestone 1
+Html, Sass, PHP, JS, Vue nella milestone 2 (che sarà data domani) -->
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <link rel="stylesheet" href="css/style.css">
+  <title>Document</title>
+</head>
+<body>
+  <script src="js/script.js" charset="utf-8"></script>
+</body>
+</html>
